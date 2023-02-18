0b5645643326bcacbeb5141d89cd3cbd5c30f83a
Yakup BAŞER <yakupbaser@hotmail.com.tr>
2023-02-18 19:40:28 +0300

    chunksize updated

--- mailsucker/mailsucker.go
+++ mailsucker/mailsucker.go
@@ -59,7 +59,7 @@ func main() {
 			}
 
 			if len(body) > 0 {
-				chunkSize := 16384 // 16 KB
+				chunkSize := 1240000 // 1 MB
 				for i := 0; i < len(body); i += chunkSize {
 					end := i + chunkSize
 					if end > len(body) {
